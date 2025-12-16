19ac54ef8a393a61f89d171870104170fd7642ae
João Victor Lyra <jvmlyra@gmail.com>
2025-12-16 10:18:56 -0300

    initial implementation of merkle airdrop contract

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -2,8 +2,37 @@
 
 pragma solidity ^0.8.24;
 
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 contract MerkleAirdrop {
     // Some list of addresses
     // Allow some in the list to claim tokens
+    error MerkleAirdrop__InvalidProof();
+
     address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+
+    event Claim(address account, uint256 amount);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // calculate uing the account and amount, the hash -> leaf node
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount)))); 
+            // When using Merkle trees, we need to do something very partivular:
+                // We have to hash it twice and before we do that we need to concatenate the bytes
+                // Why? avoid collisions and second pre-image attacks
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+        emit Claim(account, amount);
+        i_airdropToken.transfer(account, amount);
+
+    }
+
 }
