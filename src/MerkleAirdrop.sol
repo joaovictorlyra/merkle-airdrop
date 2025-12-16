7d69462e6b0016d91957642d7309337cb5e4a76f
João Victor Lyra <jvmlyra@gmail.com>
2025-12-16 09:59:01 -0300

    project init

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+contract MerkleAirdrop {
+    // Some list of addresses
+    // Allow some in the list to claim tokens
+    address[] claimers;
+}
