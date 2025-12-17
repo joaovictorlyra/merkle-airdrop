c3b0dde47be45c4f7d9487251f7b7630b16356f8
João Victor Lyra <jvmlyra@gmail.com>
2025-12-17 11:29:45 -0300

    Merkle Tree Script

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -41,4 +41,11 @@ contract MerkleAirdrop {
         i_airdropToken.safeTransfer(account, amount);
     }
 
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
 }
